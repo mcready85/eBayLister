c4a5aff76b9d3b94442b1b8091afbfa5cf22af5c
Matthew Ready <mcr23569@email.vccs.edu>
2019-04-07 13:41:50 -0400

    Initial commit

---
+++ Controllers/eBayItemsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using eBayLister.Models;
+
+namespace eBayLister.Controllers
+{
+    public class eBayItemsController : Controller
+    {
+        private eBayListerContext db = new eBayListerContext();
+
+        // GET: eBayItems
+        public ActionResult Index()
+        {
+            return View(db.eBayItems.ToList());
+        }
+
+        // GET: eBayItems/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            eBayItem eBayItem = db.eBayItems.Find(id);
+            if (eBayItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eBayItem);
+        }
+
+        // GET: eBayItems/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: eBayItems/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ItemID,ItemSKU,Title,CategoryType,Price,Condition,HandlingTime,Duration,Format,PictureURL,Quantity,ProductDescription")] eBayItem eBayItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.eBayItems.Add(eBayItem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(eBayItem);
+        }
+
+        // GET: eBayItems/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            eBayItem eBayItem = db.eBayItems.Find(id);
+            if (eBayItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eBayItem);
+        }
+
+        // POST: eBayItems/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ItemID,ItemSKU,Title,CategoryType,Price,Condition,HandlingTime,Duration,Format,PictureURL,Quantity,ProductDescription")] eBayItem eBayItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(eBayItem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(eBayItem);
+        }
+
+        // GET: eBayItems/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            eBayItem eBayItem = db.eBayItems.Find(id);
+            if (eBayItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eBayItem);
+        }
+
+        // POST: eBayItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            eBayItem eBayItem = db.eBayItems.Find(id);
+            db.eBayItems.Remove(eBayItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/eBayItem.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace eBayLister.Models
+{
+
+
+    public class eBayItem
+    {
+        [Key]
+        public int ItemID { get; set; }
+        [Display(Name = "Item SKU")]
+        public string ItemSKU { get; set; }     // Not required but needs to be generated if not supplied
+        [Required]
+        [MaxLength(80, ErrorMessage = "Title can not be longer than 80 characters")]
+        public string Title { get; set; }    
+        [Required]
+        [Display(Name = "Category")]
+        public Category CategoryType { get; set; } 
+        [Required]
+        [DataType(DataType.Currency)]
+        [Range(1,100000,ErrorMessage = "Price cannot be greater than $100,000.")]
+        public decimal Price { get; set; } 
+        [Required]
+        [Display(Name = "Condition")]
+        public ConditionType Condition { get; set; } 
+        [Required]
+        [Display(Name = "Handling Time")]
+        [Range(0,30,ErrorMessage = "Handling time cannot be greater than 30 days.")]
+        public int HandlingTime { get; set; }   // 0 represents same day handling
+        [Required]
+        [Display(Name = "Listing Duration")]
+        [Range(1,30,ErrorMessage = "Please select an option from list.")]
+        public ListingTime Duration { get; set; } 
+        [Required]
+        [Display(Name = "Listing Type")]
+        public ListingType Format { get; set; } 
+        [Required]
+        [Display(Name = "Picture URL")]
+        public string PictureURL { get; set; }  // Will need to be adjusted to allow user to browse local files
+        [Required]
+        [Range(0,30,ErrorMessage ="Quantity cannot be greater than 1,000.")]
+        public int Quantity { get; set; }
+        [Required]
+        [Display(Name = "Product Description")]
+        public string ProductDescription { get; set; }
+
+    }
+
+    public enum ConditionType
+    {
+        New = 1,
+        Used = 0
+    }
+
+    public enum ListingType
+    {
+        Auction = 0,
+        [Display(Name = "Buy It Now")]
+        BuyItNow = 1
+    }
+
+    public enum ListingTime
+    {
+        [Display(Name = "Choose...")]
+        ZeroDay = 0, // blocked by range
+        [Display(Name = "1 Day")]
+        OneDay = 1,
+        [Display(Name = "3 Days")]
+        ThreeDay = 3,
+        [Display(Name = "5 Days")]
+        FiveDay = 5,
+        [Display(Name = "7 Days")]
+        SevenDay = 7,
+        [Display(Name = "10 Days")]
+        TenDay = 10,
+        [Display(Name = "30 Days")]
+        ThirtyDay = 30
+        // Also GoodTilCancelled as option
+    }
+
+    public enum Category
+    {
+        
+        Motors,
+        Fashion,
+        Electronics,
+        [Display(Name = "Collectibles & Art")]
+        CollectiblesArt,
+        [Display(Name = "Home & Garden")]
+        HomeGarden,
+        [Display(Name = "Sporting Goods")]
+        SportingGoods,
+        [Display(Name = "Toys & Hobbies")]
+        ToysHobbies,
+        [Display(Name = "Business & Industrial")]
+        BusinessIndustrial,
+        Music
+
+    }
+
+}
+
+/*
+ * 
+ Other things necessary for making API Call that should be set in user profile include:
+ Country (assume US)
+ Currency (assume USD)
+ Postal Code (origin)
+ Return Policy (whether or not returns are accepted, if so, days to return and method of return payment)
+ Shipping Policy (UPS, USPS, FedEx, calculated or free)
+ * 
+ */
